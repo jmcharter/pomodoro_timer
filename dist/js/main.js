ba2d646a298d84f2d9fb817dbd769c2b0dcffcf0
Jon Charter <jmcharter@gmail.com>
2020-01-07 01:14:04 +0000

    made timer adjustable and resetable, made layout into a grid

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,15 +2,19 @@
 const btnStart = document.querySelector('.action-start');
 const btnPause = document.querySelector('.action-pause');
 const btnStop = document.querySelector('.action-stop');
-const clockFace = document.querySelector('.pomodoro-clock-face')
+const clockFace = document.querySelector('.pomodoro-clock-face');
+const btnTimerPlus = document.querySelector('.action-workTimerPlus');
+const btnTimerMinus = document.querySelector('.action-workTimerMinus');
+const workTime = document.querySelector('#worktime');
+
 
 //Init
-let initTime = 1500;
-let initBreak = 300;
+let initTime = 1500; //seconds
+let shortBreak = 300; //seconds
 let secondsRemaining = 1500;
 let paused = true;
 let onBreak = true;
-let timer = 0;
+let timer = 0; //variable for setInterval
 
 const alarm = document.createElement('audio');
 alarm.setAttribute("src", "http://bbcsfx.acropolis.org.uk/assets/07070161.wav");
@@ -25,16 +29,42 @@ btnPause.addEventListener('click', (event) => {
     toggleTimer();
 });
 btnStop.addEventListener('click', (event) => {
-    toggleTimer("stop");
+    toggleTimer(true);
+});
+btnTimerPlus.addEventListener('click', (event) => {
+    adjustTimer(1);
+});
+btnTimerMinus.addEventListener('click', (event) => {
+    adjustTimer(0);
 });
 
-function toggleTimer() {
-    if (paused) {
+
+function toggleTimer(checkStop) {
+    if (checkStop) {
+        clearInterval(timer);
+        secondsRemaining = initTime;
+    }
+    else if (paused) {
         paused = false;
         timer = setInterval(clockCountdown, 1000)
     } else {
         paused = true;
+        clearInterval(timer);
     }
+}
+
+function adjustTimer(increment) {
+    if (increment) {
+        initTime += 300
+    } else if (initTime > 300) {
+        initTime -= 300
+    }
+
+    let workTimerSetting = Math.floor(initTime / 60);
+
+    //conditional adds leading zero when seconds are a single digit.
+    workTime.textContent = workTimerSetting + " Minutes";
+
 }
 
 function clockCountdown() {
@@ -45,6 +75,7 @@ function updateDisplay() {
     let mins = Math.floor(secondsRemaining / 60);
     let secs = secondsRemaining % 60;
 
+    //conditional adds leading zero when seconds are a single digit.
     clockFace.textContent = mins + ":" + (secs > 10 ? secs : '0' + secs);
 }
 
