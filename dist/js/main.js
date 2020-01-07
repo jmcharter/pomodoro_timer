bc13d13ae43fc43a0aca3c1703f66609d7f05a99
Jon Charter <jmcharter@gmail.com>
2020-01-07 23:41:02 +0000

    added logic to change timer start value

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,14 +3,17 @@ const btnStart = document.querySelector('.action-start');
 const btnPause = document.querySelector('.action-pause');
 const btnStop = document.querySelector('.action-stop');
 const clockFace = document.querySelector('.pomodoro-clock-face');
-const btnTimerPlus = document.querySelector('.action-workTimerPlus');
-const btnTimerMinus = document.querySelector('.action-workTimerMinus');
-const workTime = document.querySelector('#worktime');
+const btnWorkTimerPlus = document.querySelector('.action-workTimerPlus');
+const btnWorkTimerMinus = document.querySelector('.action-workTimerMinus');
+const workTimeDisplay = document.querySelector('#worktime');
+const btnBreakTimerPlus = document.querySelector('.action-breakTimerPlus');
+const btnBreakTimerMinus = document.querySelector('.action-breakTimerMinus');
+const breakTimeDisplay = document.querySelector('#breaktime');
 
 
 //Init
-let initTime = 1500; //seconds
-let shortBreak = 300; //seconds
+let workTime = 1500; //seconds
+let breakTime = 300; //seconds
 let secondsRemaining = 1500;
 let paused = true;
 let onBreak = true;
@@ -31,18 +34,28 @@ btnPause.addEventListener('click', (event) => {
 btnStop.addEventListener('click', (event) => {
     toggleTimer(true);
 });
-btnTimerPlus.addEventListener('click', (event) => {
-    adjustTimer(1);
+btnWorkTimerPlus.addEventListener('click', (event) => {
+    adjustTimer(1, "work");
 });
-btnTimerMinus.addEventListener('click', (event) => {
-    adjustTimer(0);
+btnWorkTimerMinus.addEventListener('click', (event) => {
+    adjustTimer(0, "work");
+});
+btnBreakTimerPlus.addEventListener('click', (event) => {
+    adjustTimer(1, "break");
+});
+btnBreakTimerMinus.addEventListener('click', (event) => {
+    adjustTimer(0, "break");
 });
 
 
 function toggleTimer(checkStop) {
+    //When true is passed into the function, run the following. Otherwise, toggle the timer.
     if (checkStop) {
         clearInterval(timer);
-        secondsRemaining = initTime;
+        secondsRemaining = workTime;
+        alarm.pause();
+        alarm.currentTime = 0;
+        paused = true;
     }
     else if (paused) {
         paused = false;
@@ -53,22 +66,27 @@ function toggleTimer(checkStop) {
     }
 }
 
-function adjustTimer(increment) {
+function adjustTimer(increment, btnType) {
     if (increment) {
-        initTime += 300
-    } else if (initTime > 300) {
-        initTime -= 300
-    }
+        workTime += 300;
+    } else if (workTime > 300) {
+        workTime -= 300
+    };
 
-    let workTimerSetting = Math.floor(initTime / 60);
-
-    //conditional adds leading zero when seconds are a single digit.
-    workTime.textContent = workTimerSetting + " Minutes";
-
+    let timerSetting = Math.floor(workTime / 60);
+    workTimeDisplay.textContent = timerSetting + " Minutes";
 }
 
 function clockCountdown() {
     secondsRemaining--;
+    if (secondsRemaining <= 0) {
+        clearInterval(timer)
+        alarm.currentTime = 0; //Start audio file from beginnning.
+        alarm.play();
+
+        if (!onBreak) { breakCount++ }
+        onBreak = !onBreak; //Toggle onBreak status.
+    }
 }
 
 function updateDisplay() {
